2731db991a94088a0a8963076025c5e08a911eb3
lhotakj <lhotakj@gmail.com>
2020-03-05 00:08:28 +0100

    more elaborated example

--- main.py
+++ main.py
@@ -7,11 +7,23 @@ def group_create():
     pass
 
 
-@group_create.command(help='\033[38;5;133mCreates instance\033[0m')
-@click.option('--name', '-n', required=True, default=None, help='name of the instance', type=str)
-def create(name: str):
-    """\033[92mCreates\033[0m an EC2 instance"""
-    print(f'creating new instance {name}')
+@group_create.command(
+    help='\033[38;5;133mCreates instance\033[0m - Creates a new instance according the precept with optional override parameters')
+@click.option('--precept', '-p', required=True, default=None, help='\033[96mName of the precept\033[0m', type=str,
+              metavar="\033[32m<precept>\033[0m")
+@click.option('--force-recreate', '-f', required=False, default=False, is_flag=True,
+              help='\033[38;5;73mIn case if the instance already exists it will shut it down\033[0m')
+@click.option('--name', '-n', required=False, default=None,
+              help='\033[38;5;73mOptional name of the instance, if not defined it\'s taken from the precept\033[0m',
+              type=str,
+              metavar="\033[32m<instance-id>\033[0m")
+@click.option('--wait', '-w', default=False, is_flag=True,
+              help='\033[38;5;73mWaits until operation completes, by default it sends the signal and exits\033[0m')
+@click.option('--verbose', '-v', required=False, default=False, is_flag=True,
+              help='\033[38;5;73mShow debug info\033[0m')
+@click.help_option('--help', '-h', help='\033[38;5;73mShows this message and exits\033[0m')
+def create(precept: str, force_recreate: bool, name: str, wait: bool, verbose: bool):
+    print(f'creating new instance with name:{name} according precept:{precept}, wait:{wait}, verbose:{verbose}')
 
 
 @click.group()
@@ -19,11 +31,16 @@ def group_stop():
     pass
 
 
-@group_stop.command(help='\033[38;5;134mStops an instance\033[0m')
-@click.option('--instance-id', '-i', required=True, default=None, help='\033[32mid of the instance\033[0m', type=str)
-def stop(name: str):
-    """\033[92mCreates\033[0m an EC2 instance"""
-    print(f'creating new instance {name}')
+@group_stop.command(
+    help='\033[38;5;134mStops an instance\033[0m - Sends a signal to AWS to stop instance \033[32m<instance-id>\033[0m')
+@click.argument('instance-id', required=True, default=None, type=str, metavar="\033[32m<instance-id>\033[0m")
+@click.option('--wait', '-w', default=False, is_flag=True,
+              help='\033[38;5;73mWaits until operation completes, by default it sends the signal and exits\033[0m')
+@click.option('--verbose', '-v', required=False, default=False, is_flag=True,
+              help='\033[38;5;73mShow debug info\033[0m')
+@click.help_option('--help', '-h', help='\033[38;5;73mShows this message and exits\033[0m')
+def stop(instance_id: str, wait: bool, verbose: bool):
+    print(f'stopping an instance {instance_id} with verbose:{verbose} and wait:{wait}')
 
 
 @click.group()
@@ -31,18 +48,23 @@ def group_terminate():
     pass
 
 
-@group_terminate.command(help='\033[38;5;135mTerminates an instance\033[0m')
-@click.option('--instance-id', '-i', required=True, default=None, help='\033[32mid of the instance\033[0m', type=str)
-def terminate(instance_id: str):
-    """\033[92mTerminates\033[0m an EC2 instance"""
-    print(f'terminating new instance {instance_id}')
+@group_terminate.command(
+    help='\033[38;5;135mTerminates an instance\033[0m - Sends a signal to AWS to terminate instance \033[32m<instance-id>\033[0m')
+@click.argument('instance-id', required=True, default=None, type=str, metavar="\033[32m<instance-id>\033[0m")
+@click.option('--wait', '-w', default=False, is_flag=True,
+              help='\033[38;5;73mWaits until operation completes, by default it sends the signal and exits\033[0m')
+@click.option('--verbose', '-v', required=False, default=False, is_flag=True,
+              help='\033[38;5;73mShow debug info\033[0m')
+@click.help_option('--help', '-h', help='\033[38;5;73mShows this message and exits\033[0m')
+def terminate(instance_id: str, wait: bool, verbose: bool):
+    print(f'terminating an instance {instance_id} with verbose:{verbose} and wait:{wait}')
 
 
-@click.command(cls=click.CommandCollection, sources=[group_create, group_stop, group_terminate])
-@click.help_option(help='\033[38;5;71mshows this message and exits\033[0m')
-@click.version_option(help='\033[38;5;72mshow version info\033[0m')
-@click.option('--debug', default=False, is_flag=True, help='\033[38;5;73mshow debug info\033[0m')
-def cli(debug):
+@click.command(cls=click.CommandCollection, sources=[group_create, group_stop, group_terminate],
+               context_settings=dict(max_content_width=150))
+@click.help_option(help='\033[38;5;71mShows this message and exits\033[0m')
+@click.version_option(help='\033[38;5;72mShow version info and exists\033[0m')
+def cli():
     pass
 
 
